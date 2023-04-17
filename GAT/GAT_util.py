d0929ed7154e5205fcc45e85ebdb0f99fe32ea8b
SeongwonHwang <sw4rsw4r@gmail.com>
2023-04-17 15:38:25 +0100

    GAT

--- GAT/GAT_util.py
+++ GAT/GAT_util.py
@@ -1,7 +1,7 @@
 from networkx import *
 import torch
 import torch_geometric.transforms as T
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GATConv
 from torch_geometric.data import Data
 from torch_geometric.utils import negative_sampling
 from torch_geometric.utils import to_networkx
@@ -42,18 +42,15 @@ def build_graph(path_files, TEST_ID, type):
 class Net(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels):
         super().__init__()
-        self.conv1 = GCNConv(in_channels, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, out_channels)
-
+        self.conv1 = GATConv(in_channels, hidden_channels *2, heads=2)
+        self.conv2 = GATConv(hidden_channels *4, out_channels, heads=2)
     def encode(self, x, edge_index):
         x = self.conv1(x, edge_index).relu()
         return self.conv2(x, edge_index)
-
     def decode(self, z, edge_label_index):
         return (z[edge_label_index[0]] * z[edge_label_index[1]]).sum(
             dim=-1
         )  # product of a pair of nodes on each edge
-
     def decode_all(self, z):
         prob_adj = z @ z.t()
         return (prob_adj > 0).nonzero(as_tuple=False).t()
@@ -104,7 +101,7 @@ def get_network(path_files, TEST_ID, model):
                                               header=None, sep='\t')
     dt_all_possible_edge_labels['scores'] = prediction_scores
     dt_all_possible_edge_labels.to_csv(
-        'GCN/data/'+TEST_ID+'_prediction_score.txt', sep='\t', header=None, index=False)
+        'GAT/data/'+TEST_ID+'_prediction_score.txt', sep='\t', header=None, index=False)
 
 
 def convert_to_networkx(graph, n_sample=None):
