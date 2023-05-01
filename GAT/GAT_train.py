4fb5633d91a692af9d8295007323a0ee057a6eeb
SeongwonHwang <sw4rsw4r@gmail.com>
2023-05-01 22:38:13 +0100

    Updated GAT/GAT_train.py GAT/GAT_util.py

--- GAT/GAT_train.py
+++ GAT/GAT_train.py
@@ -1,43 +1,41 @@
 import os
 os.chdir('/home/seongwonhwang/Desktop/projects/git/GRN_inference_practice/')
 from GAT.GAT_util import *
-rng_seed=123
-# Set a fixed seed for reproducibility
-random.seed(rng_seed)
-torch.manual_seed(rng_seed)
-np.random.seed(rng_seed)
 
-
-TEST_ID = 'NodeAb1_ablation_Node'
-
-# 1. Build graphs for training and predicting
-# path_files = '/home/seongwonhwang/Desktop/projects/git/GRN_inference_practice/input_data_processing/data'
-path_files = '/home/seongwonhwang/Desktop/projects/git/Node_ablation_practice/GRN/data'
-graph_for_training = build_graph(path_files, TEST_ID, 'training')
-
-# 2. Split the graph into training and validation
-split = T.RandomLinkSplit(
-    num_val=0.05,
-    num_test=0.,
-    is_undirected=False,
-    add_negative_train_samples=True,
-    neg_sampling_ratio=1.0,
-)
-train_data, val_data, test_data = split(graph_for_training)
-print(train_data.edge_index)
-
-# 3. Build a model and train
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = Net(train_data.x.shape[1], 128, 64).to(device)
-optimizer = torch.optim.Adam(params=model.parameters(), lr=0.002)
-criterion = torch.nn.BCEWithLogitsLoss()
-model = train_link_predictor(
-    model, train_data, val_data, optimizer, criterion, n_epochs=5000)
-
-
-# 4. Save the final predicted model to a file
-get_network(path_files, TEST_ID, model, rng_seed)
-# > png('NodeAb1_control_Node_prediction_score_distribution.png', width=500, height=500, res=150)
-# > df = read.table('NodeAb1_control_Node_prediction_score.txt')
-# > hist(df[,3],breaks=100, xlab='score', main='Score distribution')
-# > dev.off()
+ID = 'NodeAb2'
+for rng_seed in (111, 123, 1234):
+    # Set a fixed seed for reproducibility
+    random.seed(rng_seed)
+    torch.manual_seed(rng_seed)
+    np.random.seed(rng_seed)
+    # TEST_ID = 'NodeAb1_control_Node'
+    for status in ('control', 'ablation'):
+        for cell_type in ('Early_endoderm', 'Node', "Mesoderm", "Prospective_neural_plate", "Area_opaca", "Mesodermal_neural", "Non_neural_ectoderm", "Anterior_ingressing_streak", "Germina_crescent", "PGCs"):
+            TEST_ID = ID+'_'+status+'_'+cell_type
+            # 1. Build graphs for training and predicting
+            # path_files = '/home/seongwonhwang/Desktop/projects/git/GRN_inference_practice/input_data_processing/data'
+            path_files = '/home/seongwonhwang/Desktop/projects/git/Node_ablation_practice/GRN/data'
+            graph_for_training = build_graph(path_files, TEST_ID, 'training')
+            # 2. Split the graph into training and validation
+            split = T.RandomLinkSplit(
+                num_val=0.05,
+                num_test=0.,
+                is_undirected=False,
+                add_negative_train_samples=True,
+                neg_sampling_ratio=1.0,
+            )
+            train_data, val_data, test_data = split(graph_for_training)
+            # 3. Build a model and train
+            device = torch.device(
+                'cuda' if torch.cuda.is_available() else 'cpu')
+            model = Net(train_data.x.shape[1], 128, 64).to(device)
+            optimizer = torch.optim.Adam(params=model.parameters(), lr=0.002)
+            criterion = torch.nn.BCEWithLogitsLoss()
+            model = train_link_predictor(
+                model, train_data, val_data, optimizer, criterion, TEST_ID, rng_seed, n_epochs=5000)
+            # 4. Save the final predicted model to a file
+            get_network(path_files, TEST_ID, rng_seed)
+            # > png('NodeAb1_control_Node_prediction_score_distribution.png', width=500, height=500, res=150)
+            # > df = read.table('NodeAb1_control_Node_prediction_score.txt')
+            # > hist(df[,3],breaks=100, xlab='score', main='Score distribution')
+            # > dev.off()
--- GAT/GAT_util.py
+++ GAT/GAT_util.py
@@ -11,6 +11,7 @@ import pandas as pd
 import random
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_auc_score
+import pickle
 
 
 def load_embedding(path_files, TEST_ID, type):
@@ -41,35 +42,44 @@ def build_graph(path_files, TEST_ID, type):
 class Net(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels):
         super().__init__()
-        self.conv1 = GATConv(in_channels, hidden_channels *2, heads=2)
-        self.conv2 = GATConv(hidden_channels *4, out_channels, heads=2)
+        self.conv1 = GATConv(in_channels, hidden_channels * 2, heads=2)
+        self.conv2 = GATConv(hidden_channels * 4, out_channels, heads=2)
+
     def encode(self, x, edge_index):
         x = self.conv1(x, edge_index).relu()
         return self.conv2(x, edge_index)
+
     def decode(self, z, edge_label_index):
         return (z[edge_label_index[0]] * z[edge_label_index[1]]).sum(
             dim=-1
         )  # product of a pair of nodes on each edge
+
     def decode_all(self, z):
         prob_adj = z @ z.t()
         return (prob_adj > 0).nonzero(as_tuple=False).t()
 
 
 def train_link_predictor(
-    model, train_data, val_data, optimizer, criterion, n_epochs=100
+    model, train_data, val_data, optimizer, criterion, TEST_ID, rng_seed=1234, n_epochs=100
 ):
     # Define the early stopping criteria
     best_val_auc = 0.
     patience = 500.
     num_epoche_no_improve = 0
+    with open('GAT/data/'+TEST_ID+'_rng'+str(rng_seed)+'.log', 'w') as f:
+        f.write('Start training..\n')
     for epoch in range(1, n_epochs + 1):
+        # set the model to training mode
         model.train()
+        # reset the optimizer gradients
         optimizer.zero_grad()
+        # encode the input data
         z = model.encode(train_data.x, train_data.edge_index)
         # sampling training negatives for every training epoch
         neg_edge_index = negative_sampling(
             edge_index=train_data.edge_index, num_nodes=train_data.num_nodes,
             num_neg_samples=train_data.edge_label_index.size(1), method='sparse')
+        # concatenate the edge labels for positive and negative edges
         edge_label_index = torch.cat(
             [train_data.edge_label_index, neg_edge_index],
             dim=-1,
@@ -78,34 +88,55 @@ def train_link_predictor(
             train_data.edge_label,
             train_data.edge_label.new_zeros(neg_edge_index.size(1))
         ], dim=0)
+        # decode the edge label indices
         out = model.decode(z, edge_label_index).view(-1)
+        # compute the loss
         loss = criterion(out, edge_label)
+        # backpropagate the gradients
         loss.backward()
+        # update the model parameters
         optimizer.step()
+        # evaluate the validation AUC
         val_auc = eval_link_predictor(model, val_data)
         # Check if the validation auc has improved
         if val_auc > best_val_auc:
+            # update the best validation AUC score
             best_val_auc = val_auc
+            # reset the number of epochs without improvement
             num_epoche_no_improve = 0
+            # save the best model as a pickle file
+            with open('GAT/data/'+TEST_ID+'_rng'+str(rng_seed)+'_model.pkl', 'wb') as f:
+                pickle.dump(model, f)
+            with open('GAT/data/'+TEST_ID+'_rng'+str(rng_seed)+'.log', 'a') as f:
+                f.write('..model saved\n')
+                log_text = f"Epoch: {epoch:03d}, Train Loss: {loss:.3f}, Best Val AUC: {best_val_auc:.3f}, Val AUC: {val_auc:.3f}\n"
+                f.write(log_text)
         else:
+            # increment the number of epochs without improvement
             num_epoche_no_improve += 1
         if epoch % 10 == 0:
-            print(
-                f"Epoch: {epoch:03d}, Train Loss: {loss:.3f}, Best Val AUC: {best_val_auc:.3f}, Val AUC: {val_auc:.3f}")
-        if num_epoche_no_improve > patience:
-            break
+            with open('GAT/data/'+TEST_ID+'_rng'+str(rng_seed)+'.log', 'a') as f:
+                log_text = f"Epoch: {epoch:03d}, Train Loss: {loss:.3f}, Best Val AUC: {best_val_auc:.3f}, Val AUC: {val_auc:.3f}\n"
+                f.write(log_text)
+        # if epoch > 5000 and num_epoche_no_improve > patience:
+        #     # stop training if the maximum number of epochs without improvement is reached
+        #     break
     return model
 
 
-def get_network(path_files, TEST_ID, model, rng_seed):
+def get_network(path_files, TEST_ID, rng_seed):
     graph_for_predicting = build_graph(path_files, TEST_ID, 'predicting')
     dt_all_possible_edge_index = pd.read_csv(path_files+'/'+TEST_ID+'_edge_index_for_predicting.txt',
                                              header=None, sep='\t')
     torch_all_possible_edge_index = torch.from_numpy(
         np.array(dt_all_possible_edge_index).T)
-    model.eval()
-    z = model.encode(graph_for_predicting.x, graph_for_predicting.edge_index)
-    out = model.decode(z, torch_all_possible_edge_index).view(-1).sigmoid()
+    with open('GAT/data/'+TEST_ID+'_rng'+str(rng_seed)+'_model.pkl', 'rb') as f:
+        best_model = pickle.load(f)
+    best_model.eval()
+    z = best_model.encode(graph_for_predicting.x,
+                          graph_for_predicting.edge_index)
+    out = best_model.decode(
+        z, torch_all_possible_edge_index).view(-1).sigmoid()
     prediction_scores = out.cpu().detach()
     dt_all_possible_edge_labels = pd.read_csv(path_files+'/'+TEST_ID+'_all_possible_edges.txt',
                                               header=None, sep='\t')
