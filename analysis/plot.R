2aaf0a3cbabd94e4ac920a615f37badd3b84de7c
SeongwonHwang <sw4rsw4r@gmail.com>
2023-05-15 22:57:22 +0100

    Updated analysis/plot.R and plot_utils.R for results using different rng

--- analysis/plot.R
+++ analysis/plot.R
@@ -1,10 +1,9 @@
-setwd("/home/seongwonhwang/Desktop/projects/git/GRN_inference_practice/plot")
+setwd("/home/seongwonhwang/Desktop/projects/git/GRN_inference_practice/analysis")
 source("plot_utils.R")
 
 # File path where GCN output stored
 path_input <- "/home/seongwonhwang/Desktop/projects/mogrify/Statistical\ Consulting/"
 path_tf_and_reqdgenes <- "/home/seongwonhwang/Desktop/projects/GRN_in_general/PyG/data/Anonymized_tables/Anonymized_tables/"
-TEST_ID <- "TEST4"
 
 # get all genes
 input_expr <- file.path(path_input, "Bayesian_DE/iterative_test/iterative_test/TF_experiment_expression_matrix.gz")
@@ -16,13 +15,21 @@ external_tfs <- grep("tf", genes_all, value = T)
 tfs <- get_tfs(path_tf_and_reqdgenes)
 tfs_all <- unique(c(external_tfs, tfs))
 
+TEST_ID <- "TEST3"
+lst_rng_seed <- c("", "_rng111_neg2.0", "_rng123_neg2.0", "_rng1234_neg2.0", "_rng111_neg3.0", "_rng123_neg3.0", "_rng1234_neg3.0", "_rng111_neg5.0", "_rng123_neg5.0", "_rng1234_neg5.0")
+
 # Load graph predictions
 list_predicted <- list()
 for (TYPE in c("GCN", "graphSAGE", "GAT")) {
-    list_predicted[[TYPE]] <- read_graph_prediction(TYPE, 'TEST3', cutoff = 0.8)
+    if (TYPE == "GAT") {
+        for (rng in lst_rng_seed) {
+            ID <- paste0(TEST_ID, rng)
+            list_predicted[[paste0(TYPE, rng)]] <- read_graph_prediction(TYPE, ID)
+        }
+    } else {
+        list_predicted[[TYPE]] <- read_graph_prediction(TYPE, "TEST3")
+    }
 }
-names(list_predicted)[3]= 'GAT_pseudo'
-list_predicted[['GAT']] <- read_graph_prediction(TYPE, 'TEST4', cutoff = 0.8)
 networks <- load_all_networks(list_predicted, tfs_all)
 
 df_all_possible_edges <- expand.grid(tfs_all, genes_all)
@@ -42,8 +49,14 @@ results_gtrd <- lapply(networks, function(x) {
 })
 
 # Represent metrics
-plot_f1(results_dorothea, TEST_ID, "Dorothea")
-plot_f1(results_gtrd, TEST_ID, "GTRD")
+plot_f1(results_dorothea, paste0(TEST_ID, "_rng"), "Dorothea")
+plot_f1(results_gtrd, paste0(TEST_ID, "_rng"), "GTRD")
+
+plot_score_distribution(dir_network = paste0("../GAT/data/"), TEST_ID)
+
+plot_scores_comp(dir_network = paste0("../GAT/data/"), TEST_ID, lst_rng_seed, type = "eigen_centrality", tfs_all)
+plot_edge_scores_comp(dir_network = paste0("../GAT/data/"), TEST_ID, lst_rng_seed)
+
 
 # plot number of edges
 plot_n_edges(networks)
--- analysis/plot_utils.R
+++ analysis/plot_utils.R
@@ -1,6 +1,8 @@
 library(igraph)
 library(ggplot2)
 library(data.table)
+library(cowplot)
+library(ggrepel)
 ##########################
 
 edge_type <- function(nt, tfs, index = c("source", "target")) {
@@ -30,15 +32,14 @@ read_network <- function(path_network, tfs_all, directed = FALSE, filter = c("TF
 }
 
 load_all_networks <- function(list_predicted, tfs_all) {
-  path_networks <- list(
-    GCN = list_predicted[["GCN"]],
-    graphSAGE = list_predicted[["graphSAGE"]],
-    GAT_pseudo = list_predicted[["GAT_pseudo"]],
-    GAT = list_predicted[["GAT"]],
-    eBIC = "/home/seongwonhwang/Desktop/projects/git/GRN_inference_practice/BIC/edge_strength_filt.txt",
-    STRING = "/home/seongwonhwang/Desktop/projects/mogrify/Statistical\ Consulting/BIC/data/networks_anonymize.txt",
-    GTRD = "/home/seongwonhwang/Desktop/projects/GRN_in_general/PyG/data/Anonymized_tables/Anonymized_tables/gtrd_anonymized.txt",
-    Dorothea = "/home/seongwonhwang/Desktop/projects/GRN_in_general/PyG/data/Anonymized_tables/Anonymized_tables/dorothea_anonymized.txt"
+  path_networks <- c(
+    list_predicted,
+    list(
+      eBIC = "/home/seongwonhwang/Desktop/projects/git/GRN_inference_practice/BIC/edge_strength_filt.txt",
+      STRING = "/home/seongwonhwang/Desktop/projects/mogrify/Statistical\ Consulting/BIC/data/networks_anonymize.txt",
+      GTRD = "/home/seongwonhwang/Desktop/projects/GRN_in_general/PyG/data/Anonymized_tables/Anonymized_tables/gtrd_anonymized.txt",
+      Dorothea = "/home/seongwonhwang/Desktop/projects/GRN_in_general/PyG/data/Anonymized_tables/Anonymized_tables/dorothea_anonymized.txt"
+    )
   )
 
   networks <- sapply(path_networks, function(x) read_network(x, tfs_all, directed = T), simplify = F)
@@ -64,11 +65,14 @@ plot_f1 <- function(results_f1, TEST_ID, type) {
     facet_wrap(~metric, ncol = 3, scales = "free") +
     geom_label(aes(label = round(value * 100, 2)), col = "white") +
     theme(axis.text.x = element_blank())
-  pdf(fig_name, width = 6.5, height = 3.5)
+  fig_width <- max(6.5, length(results_f1) * 1.5)
+  pdf(fig_name, width = fig_width, height = 3.5)
   plot(p)
   dev.off()
   return(p)
 }
+
+
 plot_n_edges <- function(networks) {
   p_df <- data.frame(TYPE = names(networks), n_edges = sapply(networks, function(x) nrow(x$edges)))
   p <- ggplot(subset(p_df, TYPE != "GTRD"), aes(x = TYPE, y = n_edges, fill = TYPE)) +
@@ -85,10 +89,165 @@ get_tfs <- function(path_tf_and_reqdgenes) {
   read.table(input_tfs)[, 1]
 }
 
-read_graph_prediction <- function(TYPE, TEST_ID, cutoff) {
+read_graph_prediction <- function(TYPE, TEST_ID, cutoff = NULL) {
   # Load prediction results
   fname_prediction_scores <- paste0("../", TYPE, "/data/", TEST_ID, "_prediction_score.txt")
   df_prediction_scores <- read.table(fname_prediction_scores)
   colnames(df_prediction_scores) <- c("TF", "target", "score")
+  if (is.null(cutoff)) cutoff <- quantile(df_prediction_scores$score, .7)
   df_prediction_scores_filt <- subset(df_prediction_scores, score > cutoff)
+}
+
+
+
+plot_score_distribution <- function(dir_network, TEST_ID) {
+  lst_predictions <- list.files(path = dir_network, pattern = "_prediction_score.txt")
+  lst_predictions <- grep(TEST_ID, lst_predictions, value = T)
+
+  lst_plots <- list()
+  for (fname_pred in lst_predictions) {
+    ID <- sub("_prediction_score.txt", "", fname_pred)
+    df_pred <- read.table(file.path(dir_network, fname_pred), sep = "\t")
+
+    colnames(df_pred) <- c("source", "target", "score")
+    cutoff <- quantile(df_pred$score, .7)
+    p <- ggplot(df_pred, aes(score)) +
+      geom_histogram() +
+      ggtitle(ID, subtitle = paste0("cutoff: ", round(cutoff, 2))) +
+      geom_vline(xintercept = cutoff, lty = 2, colour = "red") +
+      theme_classic() +
+      coord_cartesian(xlim = c(0, 1))
+    lst_plots[[ID]] <- p
+  }
+  fig_height <- max(length(lst_plots) / 3 * 500, 1000)
+  png(paste0("data/score_distribution_", TEST_ID, ".png"), width = 1400, height = fig_height, res = 150)
+  p <- plot_grid(plotlist = lst_plots, ncol = 2)
+  plot(p)
+  dev.off()
+}
+
+
+plot_scores <- function(df_scores, xlabel, ylabel, title, show_genenames = T) {
+  p <- ggplot(df_scores, aes(x = score_1, y = score_2))
+
+  if ("is_TF" %in% colnames(df_scores)) {
+    p <- p + geom_point(aes(colour = is_TF), size = 2, alpha = .6) +
+      scale_colour_manual(values = c("grey10", "red"))
+  } else {
+    p <- p + geom_point(size = 2, alpha = .6)
+  }
+  if (show_genenames) {
+    # geom_text_repel(
+    #   data = subset(df_scores, gene %in% tfnames & score_diff >= sort(subset(df_scores, gene %in% tfnames)$score_diff, decreasing = T)[3]),
+    #   aes(label = gene), size = 4, nudge_y = .15
+    # ) +
+    p <- p +
+      geom_text_repel(
+        data = subset(df_scores, score_diff >= sort(df_scores$score_diff, decreasing = T)[3]),
+        aes(label = gene), size = 4, nudge_y = -.15
+      ) +
+      geom_text_repel(
+        data = subset(df_scores, score_diff <= sort(df_scores$score_diff, decreasing = F)[3]),
+        aes(label = gene), size = 4, nudge_y = .15
+      )
+  }
+  p <- p +
+    theme_classic() +
+    geom_abline(slope = 1, intercept = 0, lty = 2, colour = "red") +
+    xlab(xlabel) +
+    ylab(ylabel) +
+    ggtitle(title)
+
+  return(p)
+}
+
+build_igraph <- function(fname_network) {
+  df_net <- read.table(fname_network, sep = "\t")
+  colnames(df_net) <- c("TF", "target", "score")
+
+  # df_net_subset <- subset(df_net, TF %in% tfnames, select = c("TF", "target"))
+  g <- igraph::graph_from_data_frame(df_net, directed = T)
+  E(g)$weight <- df_net$score
+
+  return(list(g = g, net = df_net))
+}
+
+
+compare_scores <- function(g1, g2, tfnames, type) {
+  if (type == "eigen_centrality") {
+    lst_scores1 <- eigen_centrality(g1, weights = E(g1)$weights)$vector
+    lst_scores2 <- eigen_centrality(g2, weights = E(g2)$weights)$vector
+  } else if (type == "degree") {
+    lst_scores1 <- degree(g1)
+    lst_scores2 <- degree(g2)
+  }
+
+  df_scores <- merge(
+    data.frame(gene = names(lst_scores1), score = lst_scores1),
+    data.frame(gene = names(lst_scores2), score = lst_scores2),
+    by = "gene", suffixes = c("_1", "_2")
+  )
+
+  df_scores <- transform(df_scores,
+    is_TF = gene %in% tfnames,
+    score_diff = score_1 - score_2
+  )
+  return(df_scores)
+}
+
+plot_scores_comp <- function(
+    dir_network, TEST_ID, lst_rng_seed, type, tfnames) {
+  lst_plots <- list()
+  for (idx1 in 1:(length(lst_rng_seed) - 1)) {
+    for (idx2 in idx1:length(lst_rng_seed)) {
+      rng_seed1 <- lst_rng_seed[idx1]
+      rng_seed2 <- lst_rng_seed[idx2]
+
+      if (idx1 == idx2) next
+      ID <- paste0(sub("^_", "", rng_seed1), "_vs_", sub("^_", "", rng_seed2))
+      print(ID)
+      net1 <- build_igraph(paste0(dir_network, "/", TEST_ID, rng_seed1, "_prediction_score.txt"))
+      net2 <- build_igraph(paste0(dir_network, "/", TEST_ID, rng_seed2, "_prediction_score.txt"))
+
+      df_scores <- compare_scores(net1$g, net2$g, tfnames, type)
+      lst_plots[[ID]] <- plot_scores(df_scores,
+        xlabel = sub("^_", "", rng_seed1), ylabel = sub("^_", "", rng_seed2),
+        title = paste0(type, "\n", ID)
+      )
+    }
+  }
+  fig_height <- max(length(lst_plots) / 3 * 500, 1300)
+  png(paste0("data/", type, "_comp_between_rng_", TEST_ID, ".png"), width = 2100, height = fig_height, res = 150)
+  p <- plot_grid(plotlist = lst_plots, ncol = 3)
+  plot(p)
+  dev.off()
+}
+
+plot_edge_scores_comp <- function(
+    dir_network, TEST_ID, lst_rng_seed) {
+  lst_plots <- list()
+  for (idx1 in 1:(length(lst_rng_seed) - 1)) {
+    for (idx2 in idx1:length(lst_rng_seed)) {
+      rng_seed1 <- lst_rng_seed[idx1]
+      rng_seed2 <- lst_rng_seed[idx2]
+
+      if (idx1 == idx2) next
+      ID <- paste0(sub("^_", "", rng_seed1), "_vs_", sub("^_", "", rng_seed2))
+      print(ID)
+      net1 <- build_igraph(paste0(dir_network, "/", TEST_ID, rng_seed1, "_prediction_score.txt"))
+      net2 <- build_igraph(paste0(dir_network, "/", TEST_ID, rng_seed2, "_prediction_score.txt"))
+
+      df_scores <- merge(net1$net, net2$net, by = c("TF", "target"), suffixes = c("_1", "_2"))
+      cor_val <- with(df_scores, cor(score_1, score_2))
+      lst_plots[[ID]] <- plot_scores(df_scores,
+        xlabel = sub("^_", "", rng_seed1), ylabel = sub("^_", "", rng_seed2),
+        title = paste0("Edge scores\n", ID, "\ncor: ", round(cor_val, 2)), show_genenames = F
+      )
+    }
+  }
+  fig_height <- max(length(lst_plots) / 3 * 500, 1300)
+  png(paste0("data/Edge_scores_comp_between_rng_", TEST_ID, ".png"), width = 2100, height = fig_height, res = 150)
+  p <- plot_grid(plotlist = lst_plots, ncol = 3)
+  plot(p)
+  dev.off()
 }
